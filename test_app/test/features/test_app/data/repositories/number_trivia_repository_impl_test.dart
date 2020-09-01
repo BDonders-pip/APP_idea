e9dc61bd7d5c28df6cb2e78985f16236f1fe0816
Benjamín Donders <benjadonders@gmail.com>
2020-09-01 08:42:01 -0400

    Local and Remote repositories

---
+++ test_app/test/features/test_app/data/repositories/number_trivia_repository_impl_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import "package:mockito/mockito.dart";
+import 'package:test_app/core/platform/network_info.dart';
+import 'package:test_app/features/test_app/data/datasources/number_trivia_local_data_source.dart';
+import 'package:test_app/features/test_app/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:test_app/features/test_app/data/repositories/number_trivia_repository_impl.dart';
+
+class MockRemoteDataSource extends Mock implements NumberTriviaRemoteDataSource {}
+
+class MockLocalDataSource extends Mock implements NumberTriviaLocalDataSource {}
+
+class MockNetworkInfo extends Mock implements NetworkInfo {}
+
+void main() {
+  NumberTriviaRepositoryImpl repository;
+  MockRemoteDataSource mockRemoteDataSource;
+  MockLocalDataSource mockLocalDataSource;
+  MockNetworkInfo mockNetworkInfo;
+
+  setUp(() {
+    mockRemoteDataSource = MockRemoteDataSource();
+    mockLocalDataSource = MockLocalDataSource();
+    mockNetworkInfo = MockNetworkInfo();
+    repository = NumberTriviaRepositoryImpl(
+      remoteDataSource: mockRemoteDataSource,
+      localDataSource: mockLocalDataSource,
+      networkInfo: mockNetworkInfo,
+    );
+  });
+}
