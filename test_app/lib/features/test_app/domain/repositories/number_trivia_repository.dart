c54827838cc119c65b4583c4d9b8a098658339e3
Benjamín Donders <benjadonders@gmail.com>
2020-09-03 11:28:05 -0400

    Remove the test_app due it in a new repository

--- test_app/lib/features/test_app/domain/repositories/number_trivia_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:test_app/core/error/failures.dart';
-import 'package:test_app/features/test_app/domain/entities/number_trivia.dart';
-
-abstract class NumberTriviaRepository {
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
-  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
-}
